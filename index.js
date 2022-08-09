339d22f54aa5a35812daa111e01a7fbb4d065d65
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-08-08 23:23:48 -0400

    commit tree 795b1c3a8ae4fe26264fa6fa0dc1da6a3d5640d5

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+let start;
+let destination;
+
+function distanceFromHqInBlocks (pickup) {
+    if (pickup>42){
+        return pickup-42;
+    } else return 42-pickup;
+}
+
+function distanceFromHqInFeet (feet) {
+    return distanceFromHqInBlocks(feet)*264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    if (start<destination) {
+    return (destination-start)*264;
+    } else {
+        return (start-destination)*264;
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    let feet = distanceTravelledInFeet(start,destination);
+
+    if (feet<=400){
+        return 0;
+    } else if (feet>400 && feet<2000){
+        return (feet-400)*.02;
+    } else if (feet<=2500){
+        return 25;
+    }
+    else if (feet>2500) {
+        return 'cannot travel that far';
+    }
+
+}
